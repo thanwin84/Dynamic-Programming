46bda55365007646938f8ebc94120c1b23aa898c
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-06-12 15:56:09 +0600

    Update knapsack01.cpp

--- knapsack0-1/knapsack01.cpp
+++ knapsack0-1/knapsack01.cpp
@@ -57,3 +57,29 @@ public:
     
 };
    
+//top down approach
+const int D = 1001;
+int  dp[D][D];
+class Solution
+{
+public:
+    //Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int W, int wt[], int val[], int n)
+    {
+        // Your code here
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= W; j++) {
+                if (i == 0 || j == 0) dp[i][j] = 0;
+                else if (wt[i - 1] <= j) {
+                    int included = val[i - 1] + dp[i - 1][j - wt[i - 1]];
+                    int not_included = dp[i - 1][j];
+                    dp[i][j] = max(included, not_included);
+                }
+                else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][W];
+    }
+};
