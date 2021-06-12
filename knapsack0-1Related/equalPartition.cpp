a6e9b17953e2418e10e73a78711e647bd354fb3f
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-06-12 21:33:47 +0600

    Update equalPartition.cpp

--- knapsack0-1Related/equalPartition.cpp
+++ knapsack0-1Related/equalPartition.cpp
@@ -1,3 +1,35 @@
+//memoization 
+int const D = 10000;
+int dp[201][200001];
+class Solution {
+public:
+    bool solve(vector<int>& nums, int n, int target) {
+
+        if (target < 0 || n == 0) return false;
+        if (target == 0) return true;
+        if (dp[n][target] != -10) return dp[n][target];
+        return dp[n][target] = solve(nums, n - 1, target - nums[n - 1]) or solve(nums, n - 1, target);
+    }
+    bool canPartition(vector<int>& nums) {
+        int sum = 0;
+        for (auto i : nums) {
+            sum += i;
+        }
+        for (int i = 0; i <= nums.size(); i++) {
+            for (int j = 0; j <= sum / 2; j++) {
+                dp[i][j] = -10;
+            }
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        else {
+            return solve( nums, nums.size(), sum / 2);
+        }
+
+    }
+};
+
 //top down
 int const D = 10000;
 bool dp[D][D];
