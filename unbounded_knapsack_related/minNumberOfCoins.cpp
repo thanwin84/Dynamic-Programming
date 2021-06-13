8187e48c806b03471262d4ca673a0b02e4bbc8ad
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-06-14 00:14:03 +0600

    Update minNumberOfCoins.cpp

--- unbounded_knapsack_related/minNumberOfCoins.cpp
+++ unbounded_knapsack_related/minNumberOfCoins.cpp
@@ -46,3 +46,29 @@ int main() {
     Solution obj;
     cout << obj.minCoins(arr, 3, 5);
 }
+//space optimized-O(V)
+class Solution{
+
+	public:
+	int minCoins(int coins[], int M, int V)
+	{
+		// Your code goes here
+		dp[0] = 0;
+		for (int i = 1; i <= V; i++) dp[i] = INT_MAX-1;
+		for (int i = 1; i <= V; i++) {
+			for (int j = 0; j < M; j++) {
+				if (coins[j] <= i) {
+					dp[i] = min(1 + dp[i - coins[j]], dp[i]);
+				}
+				else continue;
+			}
+		}
+		if (dp[V] == INT_MAX - 1){
+		    return -1;
+		}
+		else {
+		    return dp[V];
+		}
+	}
+
+};
