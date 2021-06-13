657b2166c6ffb4c10094049dd0e098ee063e94c7
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-06-13 15:23:07 +0600

    Create rodcutting.cpp

---
+++ unbounded_knapsack_related/rodcutting.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <queue>
+#include<set>
+#include<unordered_map>
+#include<string>
+using namespace std;
+int dp[1002][1002];
+class Solution {
+public:
+    int rod_cutting(vector<int>& prices, vector<int> &lenghts, int length) {
+        int N = prices.size();
+        for (int i = 0; i <= length; i++) dp[0][i] = 0;
+        for (int i = 0; i <= N; i++) dp[i][0] = 0;
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j <= length; j++) {
+                if (lenghts[i - 1] <= j) {
+                    dp[i][j] = max(prices[i - 1] + dp[i][j - lenghts[i - 1]], dp[i - 1][j]);
+                }
+                else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[N][N];
+    }
+};
+int main() {
+    vector<int> l = { 1, 2, 3, 4, 5 , 6,  7,  8 };
+    vector<int> p = { 1, 5, 8, 9, 10, 17, 17, 20 };
+    Solution obj;
+    cout << obj.rod_cutting(p, l, 8)<< endl;
+	return 0;
+}
