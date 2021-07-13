023f636722a624931c460ca5733a8346f54eda3b
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-07-13 21:38:46 +0600

    Update palindromePartioning.cpp

--- mcm/palindromePartioning.cpp
+++ mcm/palindromePartioning.cpp
@@ -197,3 +197,63 @@ public:
 
 	}
 };
+//space optimized
+// when we are storing the minimum cuts in dp, the end is always same. so we can avoid using 2d array for dp
+int dp[2002];
+int dp1[2002][2001];
+class Solution {
+public:
+	bool isPalindrome(string& s, int i, int j) {
+		if (i >= j) return true;
+		if (dp1[i][j] != -1) return dp1[i][j];
+		if (s[i] == s[j]) {
+			return dp1[i][j] = isPalindrome(s, i + 1, j - 1);
+		}
+		else {
+			return dp1[i][j] = false;
+		}
+	}
+	int solve(string &S, int i, int j) {
+		if (dp[i] != -1) {
+			return dp[i];
+		}
+		if (i == j) {
+			return dp[i]= 0;
+			
+		}
+		if (isPalindrome(S, i, j)) {
+			return dp[i] = 0;
+		}
+		int min_cut = INT_MAX;
+		int left, right;
+		for (int k = i; k < j; k++) {
+			/*An Optimization: We will make the partition only if the string till the partition
+		(till Kth position) is a valid palindrome. Because the question states that all
+		partition must be a valid palindrome. If we dont check this, we will have to
+		perform recursion on the left subproblem too (solve(str, i, k)) and	we will waste
+		a lot of time on subproblems that is not required.*/
+			if (isPalindrome(S, i, k)) {
+				if (dp[k + 1] != -1) {
+					right = dp[k + 1];
+				}
+				else {
+					right = solve(S, k + 1, j);
+					dp[k + 1] = right;
+				}
+			}
+			int temp =  1 + right;
+			min_cut = min(min_cut, temp);
+		}
+
+		return dp[i] =  min_cut;
+	}
+	int minCut(string s) {
+		int i = 0;
+		int j = s.size() - 1;
+		memset(dp, -1, sizeof(dp));
+		memset(dp1, -1, sizeof(dp1));
+		int result = solve(s, i, j);
+		return result;
+
+	}
+};
