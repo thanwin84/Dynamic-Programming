8fd51aebbce4126968b6c6cf26bceaf9cb1b36f0
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-07-31 22:01:27 +0600

    Update stickler_theif.cpp

--- stickler_theif.cpp
+++ stickler_theif.cpp
@@ -24,3 +24,20 @@ class Solution
         return FindMaxSumHelper(arr, n);
     }
 };
+// top-down approach
+int dp[10002];
+class Solution
+{
+    public:
+    //Function to find the maximum money the thief can get.
+    int FindMaxSum(int arr[], int n){
+        dp[0] = 0;
+        dp[1] = arr[0];
+        dp[2] = max(arr[0], arr[1]);
+        for (int i = 3; i <= n; i++){
+            dp[i] = max(arr[i-1] + dp[i -2], dp[i - 1]);
+        }
+        return dp[n];
+    }
+    
+};
