2782022ac4db50be8116cf291ec4e81b1c6d2deb
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-07-31 13:40:15 +0600

    Update goldmine.cpp

--- goldmine.cpp
+++ goldmine.cpp
@@ -1,20 +1,23 @@
+//time: O(n *m) and space: O(1)
 class Solution {
 public:
 	int maxGold(int n, int m, vector<vector<int>> &M)
 	{
-		// code here
+		// The last columm indicate maxgold miner can collect when he starts
+		// We will collect max gold for every index moving back column by column(starting from col == n -1)
 		int r = M.size() - 1; // row
 		int c = M[0].size() - 1; // col
 		int right, dright, ddown;
 		for (int col = c -1; col >= 0; col--) {
 			for (int row = 0; row <= r; row++) {
+				//if row is 0, we cannot go diagonally right
 				if (row == 0) {
 					dright = 0;
 				}
 				else {
 					dright = M[row - 1][col + 1];
 				}
-				
+				//In the last row, we cannot go diagonally down
 				if (row == r) {
 					ddown = 0;
 				}
@@ -25,9 +28,9 @@ public:
 				M[row][col] = M[row][col] + max(right, max(dright, ddown));
 			}
 		}
+		// The max value collected will be in the first column
 		int result = 0;
 		for (int i = 0; i <= r; i++) {
-			//cout << M[i][2] << " ";
 			if (M[i][0] > result) result = M[i][0];
 		}
 		return result;
