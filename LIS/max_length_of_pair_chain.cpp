4f22319354798ee2542d03b3149b930758f6cae5
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-08-05 20:31:03 +0600

    Create max_length_of_pair_chain.cpp

---
+++ LIS/max_length_of_pair_chain.cpp
@@ -0,0 +1,25 @@
+int dp[1002];
+bool comp(vector<int>& p1, vector<int>& p2) {
+	return p1[1] < p2[1];
+}
+
+class Solution {
+public:
+	int findLongestChain(vector<vector<int>>& pairs) {
+		sort(pairs.begin(), pairs.end());
+		dp[0] = 1;
+		int best_ans = 1;
+		for (int i = 1; i < pairs.size(); i++) {
+			dp[i] = 1;
+			for (int j = 0; j < i; j++) {
+				if (pairs[i][0] > pairs[j][1]) {
+					int possible_ans = 1 + dp[j];
+					dp[i] = max(dp[i], possible_ans);
+				}
+			}
+			best_ans = max(dp[i], best_ans);
+		}
+		return best_ans;
+		
+	}
+};
