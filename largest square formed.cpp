e1f17514df48d14e6225c9bf0c2817c99fb7eea4
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-08-09 17:07:18 +0600

    Update largest square formed.cpp

--- largest square formed.cpp
+++ largest square formed.cpp
@@ -1,3 +1,34 @@
+// Memoization
+//time: O(n*m) and space: O(n*m)
+int dp[52][52];
+class Solution {
+
+public:
+    int result;
+    int solve(int row, int col, vector<vector<int>>& mat, int n, int m) {
+        if (row <= 0 or col <= 0) {
+            return 0;
+        }
+        if (dp[row][col] != -1) {
+            return dp[row][col];
+        }
+        int diogonal = solve(row -1 , col -1, mat, n, m);
+        int left = solve(row, col -1, mat, n, m);
+        int up = solve(row -1, col, mat, n, m);
+        dp[row][col] = 0;
+        if (mat[row -1][col -1] == 1) {
+            dp[row][col] = 1 + min(diogonal, min(left, up));
+            result = max(result, dp[row][col]);
+        }
+        return dp[row][col];
+    }
+    int maxSquare(int n, int m, vector<vector<int>> mat) {
+        memset(dp, -1, sizeof(dp));
+        solve(n, m,  mat, n, m);
+        return result;
+    }
+};
+//top down
 int dp[52][52];
 class Solution {
 public:
