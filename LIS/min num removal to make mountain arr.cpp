4a04631f10b8ae28cf86eff0d8891b56c9e1d1d9
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-08-10 17:21:13 +0600

    Create min num removal to make mountain arr.cpp

---
+++ LIS/min num removal to make mountain arr.cpp
@@ -0,0 +1,42 @@
+//time: O(n^2) and space: O(n)
+using namespace std;
+class Solution {
+public:
+	vector<int> LIS(vector<int> &arr)
+	{
+		// your code here
+		int n = arr.size();
+		vector<int> dp(n + 1, 0);
+		dp[0] = 1;
+		int best = 0;
+		for (int i = 1; i < n; i++) {
+			dp[i] = 1;
+			for (int j = 0; j < i; j++) {
+				if (arr[j] > arr[i]) {
+					continue;
+				}
+				else if (arr[i] > arr[j]) {
+					int possibleAns = dp[j] + 1;
+					if (possibleAns > dp[i]) dp[i] = possibleAns;
+					if (dp[i] > best) best = dp[i];
+				}
+			}
+		}
+		return dp;
+	}
+	int minimumMountainRemovals(vector<int>& arr) {
+		vector<int> reversed_arr;
+		for (int i = arr.size() - 1; i >= 0; i--) reversed_arr.push_back(arr[i]);
+		vector<int> dp1 = LIS(arr);
+		vector<int> dp2 = LIS(reversed_arr);
+		int ans = INT_MIN;
+		for (int i = 1; i < arr.size() - 1; i++) {
+            if (dp1[i] > 1 and dp2[arr.size() - i -1] > 1){
+                int temp =dp1[i] + dp2[arr.size() - i - 1] - 1;
+			    ans = max(ans, temp);
+            }
+		}
+		return arr.size() - ans;
+	}
+	
+};
