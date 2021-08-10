2aad2558c694589648cf9bdf603916821d27f3b9
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-08-10 20:21:43 +0600

    Update min num removal to make mountain arr.cpp

--- LIS/min num removal to make mountain arr.cpp
+++ LIS/min num removal to make mountain arr.cpp
@@ -40,3 +40,58 @@ public:
 	}
 	
 };
+//time: O(nlogn)
+class Solution {
+public:
+    int next_greater_element_index(vector<int>& arr, int target) {
+        int low = 0;
+        int high = arr.size();
+        int index = 0;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] >= target) {
+                index = mid;
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return index;
+    }
+    vector<int> LIS(vector<int>& arr) {
+        vector<int> seq;
+        vector<int> dp;
+        dp.push_back(1);
+        seq.push_back(arr[0]);
+        for (int i = 1; i < arr.size(); i++) {
+            if (seq.back() < arr[i]) {
+                seq.push_back(arr[i]);
+            }
+            else if (seq.back() > arr[i]) {
+                int indx = next_greater_element_index(seq, arr[i]);
+                seq[indx] = arr[i];
+            }
+            dp.push_back(seq.size());
+        }
+        return dp;
+    }
+    int minimumMountainRemovals(vector<int>& arr) {
+        vector<int> reversed_arr;
+        for (int i = arr.size() - 1; i >= 0; i--) reversed_arr.push_back(arr[i]);
+        vector<int> dp1 = LIS(arr);
+        vector<int> dp2 = LIS(reversed_arr);
+        int ans = INT_MIN;
+        for (int i = 1; i < arr.size() - 1; i++) {
+            if (dp1[i] > 1 and dp2[arr.size() - i - 1] > 1) {
+                int temp = dp1[i] + dp2[arr.size() - i - 1] - 1;
+		// take max increasing subsequene 
+                ans = max(ans, temp);
+            }
+        }
+	// if we subtract the max LIS from array size, we will get min number of removal
+        return arr.size() - ans;
+    }
+
+};
+
