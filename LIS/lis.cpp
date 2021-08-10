4ba1e1e9f4fa6665004e21af88ad74293fcb5ab4
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-08-10 15:20:07 +0600

    Update lis.cpp

--- LIS/lis.cpp
+++ LIS/lis.cpp
@@ -26,20 +26,18 @@ public:
     }
 };
 //time: O(NlogN)
-class Solution
-{
+class Solution {
 public:
-	//Function to find length of longest increasing subsequence.
-	int bs(vector<int> &arr, int X) {
+	int next_greater_element(vector<int>& arr, int target) {
 		int low = 0;
-		int high = arr.size() - 1;
+		int high = arr.size();
 		int index = 0;
 		while (low <= high) {
 			int mid = low + (high - low) / 2;
-			if (arr[mid] >= X) {
+			if (arr[mid] >= target) {
 				index = mid;
+				// we can get better ans before mid
 				high = mid - 1;
-
 			}
 			else {
 				low = mid + 1;
@@ -47,24 +45,18 @@ public:
 		}
 		return index;
 	}
-	int longestSubsequence(int n, int a[])
-	{
-		// your code here
-		vector<int> lis;
-		lis.push_back(a[0]);
-		for (int i = 1; i < n; i++) {
-			// if the last element in lis is smaller than a[i],
-			//then a[i] will make longest increasing subsequence with lis
-			if (lis.back() < a[i]) {
-				lis.push_back(a[i]);
-			}
-			// if a[i] is bigger than lis[back], search for the next greater element than a[i]
-			// after that replace with a[i]
-			else if(lis.back() > a[i]) {
-				int index = bs(lis, a[i]);
-				lis[index] = a[i];
+	int lengthOfLIS(vector<int>& arr) {
+		vector<int> li;
+		li.push_back(arr[0]);
+		for (int i = 1; i < arr.size(); i++) {
+            // if last index of li is less than arr[i], we can push arr[i] to li
+            // which will make LIS
+			if (li.back() < arr[i]) li.push_back(arr[i]);
+			else if (li.back() > arr[i]) {
+				int next_greater_index = next_greater_element(li, arr[i]);
+				li[next_greater_index] = arr[i];
 			}
 		}
-		return lis.size();
+		return li.size();
 	}
 };
