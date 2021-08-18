53d9b627da0c02a341a79c294007d4621a34c72d
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-08-18 21:10:33 +0600

    Update best time to buy and sell stock III.cpp

--- best time to buy and sell stock III.cpp
+++ best time to buy and sell stock III.cpp
@@ -24,3 +24,36 @@ public:
         return solve(prices, 0, 2, false);
     }
 };
+//Max profit with at most two transactions =
+  //     MAX {max profit with one transaction and subarray price[0..i] +
+    //        max profit with one transaction and subarray price[i+1..n-1]  }
+//i varies from 0 to n-1.
+//time complexity of this solution is O(n2)
+// we can optimize this solution
+
+//time: O(n) and space: O(n)
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<int> left(n, 0);
+        vector<int> right(n, 0);
+        left[0] = 0; //same day transaction is not possible
+        right[n -1] = 0;
+        int left_min = prices[0];
+        int right_max = prices[n -1];
+        for(int i = 1; i < n; i++){
+            left[i] = max(left[i -1], prices[i] - left_min);
+            left_min = min(left_min, prices[i]);
+        }
+        for(int i = n -2; i >= 0; i--){
+            right[i] = max(right[i + 1], right_max - prices[i]);
+            right_max = max(right_max, prices[i]);
+        }
+        int profit = right[0];
+        for(int i = 1; i < n; i++){
+            profit = max(profit, left[i -1] + right[i]);
+        }
+        return profit;
+    } 
+};
