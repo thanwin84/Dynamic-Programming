258d6b0bb93f60d0ed2f4080677fd0d5b878cb59
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-08-22 15:45:09 +0600

    Update 4countSubset.cpp

--- knapsack0-1Related/4countSubset.cpp
+++ knapsack0-1Related/4countSubset.cpp
@@ -7,18 +7,21 @@
 #include<set>
 #include<unordered_map>
 #include<string>
+#define mod 1000000007
 using namespace std;
-int const D = 10000;
-int dp[1000][1000];
+int dp[201][10002];
 class Solution {
 public:
-    int  countSubset(vector<int>& nums, int sum) {
-        for (int i = 0; i <= nums.size(); i++) dp[0][i] = 0;
-        for (int i = 0; i <= sum; i++) dp[i][0] = 1;
+    int count_subset(vector<int>& nums, int sum) {
+      
+        
+        for (int i = 0; i <= sum; i++) dp[0][i] = 0;
+	// we can always make empty set 
+        for (int i = 0; i <= nums.size(); i++) dp[i][0] = 1;
         for (int i = 1; i <= nums.size(); i++) {
-            for (int j = 1; j <= sum; j++) {
+            for (int j = 1; j <= sum ; j++) {
                 if (nums[i - 1] <= j) {
-                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i - 1]];
+                    dp[i][j] = dp[i - 1][j - nums[i - 1]] + dp[i - 1][j];
                 }
                 else {
                     dp[i][j] = dp[i - 1][j];
@@ -30,8 +33,9 @@ public:
     }
 };
 int main() {
-    vector<int> arr = { 3,5,6,7 };
-    Solution obj;
-    cout << obj.countSubset(arr, 9)<< endl;
+	Solution ob;
+    vector<int> nums = { 1, 2, 3, 3};
+    cout << ob.count_subset(nums, 6);
+   
 	return 0;
 }
