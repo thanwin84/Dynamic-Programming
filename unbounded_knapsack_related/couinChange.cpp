13ea23360bf6f612120ac1c6834bd67085f160d9
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-08-22 21:03:13 +0600

    Update couinChange.cpp

--- unbounded_knapsack_related/couinChange.cpp
+++ unbounded_knapsack_related/couinChange.cpp
@@ -31,31 +31,34 @@ public:
     }
 };
 //time O(n*m) and space: O(n*m)
-long long int dp[1000][1000];
-class Solution
-{
-  public:
-    long long int count( int coins[], int m, int n )
-    {
-       
-        //code here.
-        for (int i = 0; i <= n; i++) {
-            dp[0][i] = 0;
+int dp[14][10002];
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        for (int i = 0; i <= amount; i++) dp[0][i] = INT_MAX -1;
+        for (int i = 0; i <= coins.size(); i++) dp[i][0] = 0;
+        for (int i = 1; i <= amount; i++){
+            if (i % coins[0] == 0){
+                dp[1][i] = i / coins[0];
+            }
+            else{
+                dp[1][i] = INT_MAX - 1;
+            }
         }
-        for (int i = 1; i <= m; i++) {
-            dp[i][0] = 1;
-        }
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (coins[i - 1] <= j) {
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+        for (int i = 2; i <= coins.size(); i++){
+            for (int j = 1; j <= amount; j++){
+                if (coins[i -1] <= j){
+                    dp[i][j] = min(1 + dp[i][j - coins[i -1]], dp[i -1][j]);
                 }
                 else {
-                    dp[i][j] = dp[i - 1][j];
+                    dp[i][j] = dp[i -1][j];
                 }
             }
         }
-        return dp[m][n];
+        if (dp[coins.size()][amount] == INT_MAX -1){
+            return -1;
+        }
+        return dp[coins.size()][amount];
     }
 };
 //coin change for leetcode
