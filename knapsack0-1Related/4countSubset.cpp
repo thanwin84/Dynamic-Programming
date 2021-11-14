a51655821dafa3be3cf859bbcf7024a08f6af36c
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-11-14 15:36:14 +0600

    Update 4countSubset.cpp

--- knapsack0-1Related/4countSubset.cpp
+++ knapsack0-1Related/4countSubset.cpp
@@ -1,3 +1,38 @@
+//memoization 
+int dp[1000][10000];
+class Solution{
+
+	public:
+	int solve(int arr[], int n, int sum){
+	    if (sum == 0){
+	        return 1;
+	    }
+	    if (n == 0){
+	        return 0;
+	    }
+	    if (dp[n][sum] != -1){
+	        return dp[n][sum];
+	    }
+	    if (arr[n - 1] <= sum){
+	        int included = solve(arr, n - 1, sum - arr[n -1]) % 1000000007;
+	        int excluded = solve(arr, n - 1, sum) % 1000000007;
+	        return dp[n][sum] = (included + excluded)  %1000000007; 
+	    }
+	    else {
+	        return dp[n][sum] = solve(arr, n - 1, sum);
+	    }
+	}
+	int perfectSum(int arr[], int n, int sum)
+	{
+        // Your code goes here
+        memset(dp, -1, sizeof(dp));
+        return solve(arr, n, sum);
+	}
+	  
+};
+
+
+//top down
 #include <iostream>
 #include <vector>
 #include <algorithm>
