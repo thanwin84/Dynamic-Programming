8c70e14e95db4e6164d6edc4b20ee0af8cfa74ec
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2021-12-20 16:51:00 +0600

    Update 03max_length_of_pair_chain.cpp

--- LIS/03max_length_of_pair_chain.cpp
+++ LIS/03max_length_of_pair_chain.cpp
@@ -20,3 +20,22 @@ public:
 		
 	}
 };
+//time: O(nlogn)
+bool comp(vector<int> &p1, vector<int> &p2){
+    return p1[1] < p2[1];
+}
+class Solution {
+public:
+    int findLongestChain(vector<vector<int>>& pairs) {
+        sort(pairs.begin(), pairs.end(), comp);
+        int ans = 0;
+        int curr = INT_MIN;
+        for (int i = 0; i < pairs.size(); i++){
+            if (curr < pairs[i][0]){
+                curr = pairs[i][1];
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
