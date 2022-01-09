dbb34898284fc1f14789c38791a01541481b6b75
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-01-09 15:26:47 +0600

    Update 1mcm.cpp

--- mcm/1mcm.cpp
+++ mcm/1mcm.cpp
@@ -24,3 +24,31 @@ public:
         return result;
     }
 };
+#bottom up
+int dp[1002][1002];
+class Solution {
+public:
+    int solve(int arr[], int i, int j) {
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        if (i >= j) {
+            return 0;
+        }
+        int ans = INT_MAX;
+        for (int k = i; k < j; k++) {
+            int left = solve(arr, i, k);
+            int right = solve(arr, k + 1, j);
+            int cost = arr[i - 1] * arr[k] * arr[j];
+            int temp = left + right + cost;
+            ans = min(ans, temp);
+        }
+        return dp[i][j] = ans;
+    }
+    int matrixMultiplication(int N, int arr[])
+    {
+        // code here
+        memset(dp, -1, sizeof(dp));
+        return solve(arr, 1, N - 1);
+    }
+};
