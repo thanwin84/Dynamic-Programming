223bb6d655f8be420cf8e0c47d64e501473297fa
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-01-19 01:33:09 +0600

    Update climbing stairs.cpp

--- climbing stairs.cpp
+++ climbing stairs.cpp
@@ -22,3 +22,20 @@ public:
         return min(dp[n-1], dp[n-2]);
     }
 };
+//iterative approach
+int dp[1002];
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        if (cost.size() == 2){
+            return min(cost[0], cost[1]);
+        }
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        int n = cost.size();
+        for(int i = 2; i < n; i++){
+            dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i];
+        }
+        return min(dp[n - 1], dp[n - 2]);
+    }
+};
