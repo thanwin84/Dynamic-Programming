8aa1b613aaa65c2f7be69d2e160ee900e8d9a659
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-01-21 16:28:33 +0600

    Update and rename house robber.cpp to 003house robber.cpp

--- 003house robber.cpp
+++ 003house robber.cpp
@@ -1,3 +1,4 @@
+//****top down****/
 int dp[101];
 class Solution {
 public:
@@ -18,3 +19,20 @@ public:
     }
 };
 //time: O(n) and space: O(n)
+
+//*********iterative appraoch*****//
+int dp[101];
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() == 1){
+            return nums[0];
+        }
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < nums.size(); i++){
+            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1]);
+        }
+        return dp[nums.size() - 1];
+    }
+};
