2b71f2f54ab6d0fa546caa845a6ca0c1e63a8d73
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-02-02 22:27:49 +0600

    Update 008delete operations for two strings.cpp

--- 008delete operations for two strings.cpp
+++ 008delete operations for two strings.cpp
@@ -24,3 +24,27 @@ public:
         return solve(word1, word2, word1.size(), word2.size());
     }
 };
+//time: O(n *m)
+int dp[501][501];
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        for (int i = 0; i <= word1.size(); i++){
+            for (int j = 0; j <= word2.size(); j++){
+                if (i == 0){
+                    dp[i][j] = j;
+                }
+                else if (j == 0){
+                    dp[i][j] = i;
+                }
+                else if (word1[i -1] == word2[j -1]){
+                    dp[i][j] = dp[i -1][j -1];
+                }
+                else {
+                    dp[i][j] = 1 + min(dp[i -1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[word1.size()][word2.size()];
+    }
+};
