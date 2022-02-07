9c044943d2ad8548920116f2434a0e78e548c817
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-02-07 17:15:30 +0600

    Update best time to buy and sell stock.cpp

--- selling stocks/best time to buy and sell stock.cpp
+++ selling stocks/best time to buy and sell stock.cpp
@@ -1,3 +1,31 @@
+//Dynamic Approach
+int dp[100002][2][2];
+class Solution {
+public:
+    int solve(vector<int>& prices, int pos, int k, bool bought) {
+        if (pos >= prices.size() || k == 0) {
+            return 0;
+        }
+        if (dp[pos][bought][k] != -1) {
+            return dp[pos][bought][k];
+        }
+        int skip = solve(prices, pos + 1, k, bought);
+        if (bought) {
+            int buy = -prices[pos] + solve(prices, pos + 1, k, false);
+            return dp[pos][bought][k]= max(buy, skip);
+        }
+        else {
+            int sell = prices[pos] + solve(prices, pos + 1, k - 1, true);
+            return dp[pos][bought][k] = max(sell, skip);
+        }
+    }
+    int maxProfit(vector<int>& prices) {
+        memset(dp, -1, sizeof(dp));
+        return solve(prices, 0, 1, true);
+    }
+};
+//single pass Solution;
+//time: O(n) and space: O(1)
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
