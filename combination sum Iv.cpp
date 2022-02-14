f87bb36c044a9f7da23dc869da0fc52916a9693f
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-02-14 15:40:52 +0600

    Update combination sum Iv.cpp

--- combination sum Iv.cpp
+++ combination sum Iv.cpp
@@ -24,4 +24,25 @@ public:
         return result;
     }
 };
-//time: O(n^2) and space: O(T) where T is the value of target
+//time: O(T*N) and space: O(T) where T is the value of target and N is the size of given array
+//iterative approach
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target + 1, 0);
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            long long int ans = 0;
+            for (int j = 0; j < nums.size(); j++) {
+                int current = nums[j];
+                if (i >= current) {
+                    ans += dp[i - current];
+                }
+            }
+            dp[i] = ans;
+           
+        }
+        return dp[target];
+    }
+};
+
