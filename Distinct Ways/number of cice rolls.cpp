efa9b86a073b643eee336b44d9d01cf8fbe28c33
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-03-01 14:58:31 +0600

    Update number of cice rolls.cpp

--- Distinct Ways/number of cice rolls.cpp
+++ Distinct Ways/number of cice rolls.cpp
@@ -28,3 +28,28 @@ public:
         return solve(k, n, target);
     }
 };
+// iterative approach
+// time and space: O(dices * target)
+#define mod 1000000007
+long long dp[31][1001];
+class Solution {
+public:
+    int numRollsToTarget(int dices, int faces, int target) {
+        dp[0][0] = 1;
+        for (int dice = 1; dice <= dices; dice++){
+            for (int amount = 1; amount <= target; amount++){
+                dp[dice][amount] = 0;
+                // try every possible faces
+                for (int face = 1; face <= faces; face++){
+                    if (face <= amount){
+                        dp[dice][amount] += (dp[dice - 1][amount - face] % mod);
+                    }
+                    
+                }
+            }
+        }
+    
+        return dp[dices][target] % mod;
+        
+    }
+};
