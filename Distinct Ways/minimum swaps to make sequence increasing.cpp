1711b5ec0ead7dd80a64cbb1a29026bb2f14a678
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-03-07 00:29:37 +0600

    Update minimum swaps to make sequence increasing.cpp

--- Distinct Ways/minimum swaps to make sequence increasing.cpp
+++ Distinct Ways/minimum swaps to make sequence increasing.cpp
@@ -24,3 +24,33 @@ public:
         return solve(nums1, nums2, 0, INT_MIN, INT_MIN, 0);
     }
 };
+// iterative approach
+// time: O(n) and space: O(n)
+int dp[100002][2];
+class Solution {
+public:
+    int minSwap(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size();
+        dp[0][0] = 0;
+        dp[0][1] = 1;
+        for (int pos = 1; pos < n; pos++){
+            int prevA = nums1[pos -1];
+            int prevB = nums2[pos -1];
+            dp[pos][0] = INT_MAX;
+            dp[pos][1] = INT_MAX;
+            if (prevA < nums1[pos] && prevB < nums2[pos]){
+                // if we have to swap ith postiton, we also have to swap i-1th, cause swapping may disturb the lis format
+                // if we don't swap, we also don't have to swap i-1th 
+                dp[pos][0] = dp[pos - 1][0];
+                dp[pos][1] = dp[pos - 1][1] + 1;
+            }
+            if (prevB < nums1[pos] && prevA < nums2[pos]){
+                // if we don't swap here, we have to swap i-th, cause adjacent element may not help to make lis
+                dp[pos][0] = min(dp[pos - 1][1], dp[pos][0]);
+                // if we swap, i - 1th doesn't need to be swapped
+                dp[pos][1] = min(dp[pos][1], dp[pos -1][0] + 1);
+            }
+        }
+        return min(dp[n -1][0], dp[n -1][1]);
+    }
+};
