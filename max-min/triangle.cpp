0e79fa35f65e72f89bbdf283f27b7f8618a92614
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-03-09 01:56:40 +0600

    Create triangle.cpp

---
+++ max-min/triangle.cpp
@@ -0,0 +1,40 @@
+int dp[201][201];
+class Solution {
+public:
+    int solve(vector<vector<int>>& triangle, int row, int col){
+        if (row >= triangle.size() || col >= triangle[row].size()){
+            return 0;
+        }
+        if (row == triangle.size() - 1 && col < triangle[row].size() && col >= 0){
+            return triangle[row][col];
+        }
+        if (dp[row][col] != -1){
+            return dp[row][col];
+        }
+        int current_ind = solve(triangle, row + 1, col);
+        int next_ind = solve(triangle, row + 1, col + 1);
+        return dp[row][col] = triangle[row][col] + min(current_ind, next_ind);
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        memset(dp, -1, sizeof(dp));
+        return solve(triangle, 0, 0);
+    }
+};
+// time: O(n + m) and space: O(row * col)
+
+//iterative approach with same time and space complexity
+int dp[201][201];
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int last_row = triangle.size();
+        int last_row_col = triangle[last_row - 1].size();
+        for (int i = 0; i < last_row_col; i++) dp[last_row -1][i] = triangle[last_row -1][i];
+        for (int row = last_row - 2; row >= 0; row--){
+            for (int col = 0; col < triangle[row].size(); col++){
+                dp[row][col] = triangle[row][col] + min(dp[row + 1][col], dp[row + 1][col + 1]);
+            }
+        }
+        return dp[0][0];
+    }
+};
