25723a92ca6db07470a141fce004fc66c25b700c
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-03-15 21:47:03 +0600

    Update ones and zeroes.cpp

--- max-min/ones and zeroes.cpp
+++ max-min/ones and zeroes.cpp
@@ -34,3 +34,35 @@ public:
         return solve(strs, strs.size() -1, m, n);
     }
 };
+// top down 
+int dp[601][101][101];
+class Solution {
+public:
+    pair<int, int> countZeroesOnes(string &str){
+        int n = str.size();
+        int zeroes = 0;
+        for (int i = 0; i < n; i++){
+            if (str[i] == '0') zeroes++;
+        }
+        return {zeroes, n - zeroes};
+    }
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        
+        for (int currentInd = 1; currentInd <= strs.size(); currentInd++){
+            pair<int, int> p = countZeroesOnes(strs[currentInd-1]);
+            for (int zeroes = 0; zeroes <= m; zeroes++){
+                for (int ones = 0; ones <= n; ones++){
+                    dp[currentInd][zeroes][ones] = 0;
+                    int including = 0;
+                    int excluding = dp[currentInd - 1][zeroes][ones];
+                    if (zeroes - p.first >= 0 && ones - p.second >= 0){
+                        including = 1 + dp[currentInd - 1][zeroes - p.first][ones - p.second];
+                    }
+                    dp[currentInd][zeroes][ones] = max(including, excluding);
+                }
+            }
+        }
+        
+        return dp[strs.size()][m][n];
+    }
+};
