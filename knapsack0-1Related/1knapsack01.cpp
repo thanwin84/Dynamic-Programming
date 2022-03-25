22a2f721ecc25e4f88d379b1d274c28c1bc17685
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-03-25 22:03:58 +0600

    Update 1knapsack01.cpp

--- knapsack0-1Related/1knapsack01.cpp
+++ knapsack0-1Related/1knapsack01.cpp
@@ -1,22 +1,26 @@
 //recursive
 class Solution
 {
-public:
+    public:
     //Function to return max value that can be put in knapsack of capacity W.
-    int knapSackRe(int W, int wt[], int val[], int n)
-    {
-        // Your code here
-        if (W == 0 or n == 0) {
-            return 0;
-        }
-        if (wt[n - 1] >= W) {
-            return max(val[n - 1] + knapSackRe(W - wt[n - 1], wt, val, n - 1), knapSackRe(W, wt, val, n - 1));
-        }
-        else {
-            return knapSackRe(W, wt, val, n - 1);
-        }
+    int solve(int W, int wt[], int val[], int n) 
+    { 
+       // Your code here
+       if (W == 0 || n == 0){
+           return 0;
+       }
+       int including = INT_MIN, not_including = INT_MIN;
+       if (wt[n -1] <= W){
+           including = val[n -1] + solve(W - wt[n -1], wt, val, n -1);
+       }
+       not_including = solve(W, wt, val, n -1);
+       return max(including, not_including);
     }
-    
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       // Your code here
+       return solve(W, wt, val, n);
+    }
 };
 //memoization
 //time: O(n*W) and space: O(n*W)
@@ -58,28 +62,72 @@ public:
 };
    
 //top down approach
-const int D = 1001;
-int  dp[D][D];
 class Solution
 {
-public:
+    public:
     //Function to return max value that can be put in knapsack of capacity W.
-    int knapSack(int W, int wt[], int val[], int n)
-    {
-        // Your code here
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= W; j++) {
-                if (i == 0 || j == 0) dp[i][j] = 0;
-                else if (wt[i - 1] <= j) {
-                    int included = val[i - 1] + dp[i - 1][j - wt[i - 1]];
-                    int not_included = dp[i - 1][j];
-                    dp[i][j] = max(included, not_included);
-                }
-                else {
-                    dp[i][j] = dp[i - 1][j];
-                }
-            }
-        }
-        return dp[n][W];
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       // Your code here
+       vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
+       for (int i = 1; i <= n; i++){
+           for (int j = 1; j <= W; j++){
+               int not_including = dp[i - 1][j];
+               int including = INT_MIN;
+               if (wt[i - 1] <= j){
+                   including = val[i - 1] + dp[i -1][j - wt[i - 1]];
+               }
+               dp[i][j] = max(including, not_including);
+           }
+       }
+       return dp[n][W];
+    }
+}
+
+// space optimized solution
+// space: O(W)
+lass Solution
+{
+    public:
+    //Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       // Your code here
+       vector<int> curr(W + 1, 0), prev(W + 1, 0);
+       for (int i = 1; i <= n; i++){
+           for (int j = 1; j <= W; j++){
+               int not_including = prev[j];
+               int including = INT_MIN;
+               if (wt[i - 1] <= j){
+                   including = val[i - 1] + prev[j - wt[i - 1]];
+               }
+               curr[j] = max(including, not_including);
+           }
+           prev = curr;
+       }
+       return prev[W];
+    }
+};
+// space Optimized to 1d
+class Solution
+{
+    public:
+    //Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       // Your code here
+       vector<int> curr(W + 1, 0);
+       for (int i = 1; i <= n; i++){
+           curr[0] = 0;
+           for (int j = W; j >= 1; j--){
+               int including = INT_MIN;
+               if (wt[i - 1] <= j){
+                   including =   val[i - 1] + curr[j - wt[i - 1]];
+               }
+               curr[j] = max(curr[j], including);
+               
+           }
+       }
+       return curr[W];
     }
 };
