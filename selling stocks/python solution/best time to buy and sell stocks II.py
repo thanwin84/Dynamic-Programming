9d21cbd41eb20bb764409d2f693782a92bd631fc
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-04-07 23:58:59 +0600

    Update best time to buy and sell stocks II.py

--- selling stocks/python solution/best time to buy and sell stocks II.py
+++ selling stocks/python solution/best time to buy and sell stocks II.py
@@ -24,3 +24,17 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         dp = [[-1 for i in range(2)] for j in range(len(prices) + 1)]
         return self.solve(prices, 0, True, dp)
+# iterative approach
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        days = len(prices)
+        profit = [[0 for i in range(2)] for j in range(days + 1)]
+        for curr in range(days -1, -1, -1):
+            for buy in range(2):
+                skip = profit[curr + 1][buy]
+                if buy:
+                    profit[curr][buy] = max(-prices[curr] + profit[curr + 1][False], skip)
+                else:
+                    profit[curr][buy] = max(prices[curr] + profit[curr + 1][True], skip)
+        return profit[0][1];
+                    
