3f0ac6c1b15228e4a35082060ea20549c9212512
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-04-14 16:19:58 +0600

    Update best time to buy and stock III.cpp

--- selling stocks/cpp solution/best time to buy and stock III.cpp
+++ selling stocks/cpp solution/best time to buy and stock III.cpp
@@ -53,3 +53,34 @@ public:
         return dp[0][0][true];
     }
 };
+// space: O(1)
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<int>> ahead(3, vector<int>(2, 0));
+        vector<vector<int>> current(3, vector<int>(2, 0));
+        for (int currDay = n; currDay >= 0; currDay--){
+            for (int trans = 0; trans <= 2; trans++){
+                for (int bought = 0; bought <= 1; bought++){
+                    if (currDay == n || trans == 2){
+                        current[trans][bought] = 0;
+                        
+                    }
+                    else if (bought){
+                        int buy = -prices[currDay] + ahead[trans][false];
+                        int skip = ahead[trans][bought];
+                        current[trans][bought] = max(buy, skip);
+                    }
+                    else {
+                        int sell = prices[currDay] + ahead[trans + 1][true];
+                        int skip = ahead[trans][bought];
+                        current[trans][bought] = max(sell, skip);
+                    }
+                }
+            }
+            ahead = current;
+        }
+        return current[0][true];
+    }
+};
