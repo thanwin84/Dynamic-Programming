ddbda4815c3c90ba396d47094ce30fef16915521
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-04-28 15:41:11 +0600

    Update best time to buy and sell stock with cooldown.cpp

--- selling stocks/cpp solution/best time to buy and sell stock with cooldown.cpp
+++ selling stocks/cpp solution/best time to buy and sell stock with cooldown.cpp
@@ -23,3 +23,32 @@ public:
         return profit[0][true];
     }
 };
+
+// space optimized solution
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int days = prices.size();
+        vector<int> ahead(2, 0), ahead2(2, 0), current(2, 0);
+        for (int day = days - 1; day >= 0; day--){
+            for (int bought = 0; bought <= 1; bought++){
+                int skip = ahead[bought];
+                if (bought){
+                    int buy = -prices[day] + ahead[false];
+                    current[bought] = max(buy, skip);
+                }
+                else {
+                    int sell = prices[day];
+                    if (day + 2 <= days) {
+                        sell = prices[day] + ahead2[true];
+                    }
+                    current[bought] = max(sell, skip);
+                }
+                
+            }
+            ahead2 = ahead;
+            ahead = current;
+        }
+        return ahead[true];
+    }
+};
