11807ffa84dad63044b467f2378f37b4f5cbd0fe
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-08-15 17:20:36 +0600

    Update house robber 2.cpp

--- Decision making/house robber 2.cpp
+++ Decision making/house robber 2.cpp
@@ -1,25 +1,32 @@
-int dp[101];
-class Solution {
-public:
-    int solve(vector<int>& nums, int start, int end) {
-        dp[start] = nums[start];
-        dp[start + 1] = max(nums[start], nums[start + 1]);
-        for (int i = start + 2; i <= end; i++) {
-            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1]);
+// coding ninja solution
+#include<bits/stdc++.h>
+using namespace std;
+long long int solve(vector<int>& nums, int start, int end) {
+    // Write your code here.
+    long long int prev = nums[start];
+    long long int sprev = 0;
+    for (int pos = start + 1; pos <= end; pos++) {
+        long long int pick = 0;
+        long long int not_pick = 0;
+        if (pos - 2 >= 0) {
+            pick = nums[pos] + sprev;
         }
-        return dp[end];
+        else pick = nums[pos];
+        not_pick = prev;
+        long long int current = max(pick, not_pick);
+        sprev = prev;
+        prev = current;
+        
     }
-    int rob(vector<int>& nums) {
-        if (nums.size() == 2) {
-            return max(nums[0], nums[1]);
-        }
-        if (nums.size() == 1) {
-            return nums[0];
-        }
-        int n = nums.size() - 1;
-        int f = solve(nums, 1, n);
-        int r = solve(nums, 0, n - 1);
-        return max(f, r);
 
-    }
-};
+    return prev;
+}
+long long int houseRobber(vector<int>& valueInHouse)
+{
+    // Write your code here.
+    int n = valueInHouse.size();
+    long long int chosing_first_house = solve(valueInHouse, 0, n - 2);
+    long long int chosing_last_house = n >= 1 ? solve(valueInHouse, 1, n - 1) : 0;
+    return max(chosing_first_house, chosing_last_house);
+    
+}
