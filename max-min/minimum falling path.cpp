6de5ff64966eaf30de3f4eafc7ca0e2e613045ff
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-09-11 15:54:51 +0600

    Update minimum falling path.cpp

--- max-min/minimum falling path.cpp
+++ max-min/minimum falling path.cpp
@@ -2,20 +2,20 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int row =matrix.size();
-        int col = matrix[0].size();
-        for (int r = row -2; r >= 0; r--){
-            for (int c = 0; c < col; c++){
-                int down = INT_MAX, down_left = INT_MAX, down_right = INT_MAX;
-                down = matrix[r + 1][c];
-                if (c - 1 >= 0) down_left = matrix[r + 1][c - 1];
-                if (c + 1 < col) down_right = matrix[r + 1][c + 1];
-                matrix[r][c] =matrix[r][c] +  min(down, min(down_left, down_right));
+        int n = matrix.size();
+        for (int currRow = n - 2; currRow >= 0; currRow--){
+            for (int currCol = 0; currCol < n; currCol++){
+                int downward = INT_MAX, ldiagonal = INT_MAX, rdiagonal = INT_MAX;
+                downward = matrix[currRow + 1][currCol];
+                if (currCol - 1 >= 0) ldiagonal = matrix[currRow + 1][currCol - 1];
+                if (currCol + 1 < n) rdiagonal = matrix[currRow + 1][currCol + 1];
+                matrix[currRow][currCol] += min(downward, min(ldiagonal, rdiagonal));
+                
             }
         }
         int ans = INT_MAX;
-        for (int c = 0; c < row; c++){
-            ans = min(matrix[0][c], ans);
+        for (int i = 0; i < n; i++){
+            ans = min(ans, matrix[0][i]);
         }
         return ans;
     }
