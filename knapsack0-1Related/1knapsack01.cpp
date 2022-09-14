5ae24c0717bc6adc0054f6945040df3cfd22b168
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-09-14 15:04:26 +0600

    Update 1knapsack01.cpp

--- knapsack0-1Related/1knapsack01.cpp
+++ knapsack0-1Related/1knapsack01.cpp
@@ -24,41 +24,34 @@ class Solution
 };
 //memoization
 //time: O(n*W) and space: O(n*W)
-const int D = 1001;
-int  dp[D][D];
+int dp[1001][1001];
 class Solution
 {
 public:
     //Function to return max value that can be put in knapsack of capacity W.
-    int knapSackRe(int W, int wt[], int val[], int n)
-    {
-        // Your code here
-        if (n == 0 || W == 0) {
+    int solve(int W, int wt[], int val[], int n) {
+        if (n == 0) {
+            if (wt[0] <= W) return val[0];
             return 0;
         }
+        int pick = 0, notPick = 0;
         if (dp[n][W] != -1) {
             return dp[n][W];
         }
-        else {
-            if (wt[n - 1] <= W) {
-                dp[n][W] = max(val[n - 1] + knapSackRe(W - wt[n - 1], wt, val, n - 1), knapSackRe(W, wt, val, n - 1));
-            }
-            else {
-                dp[n][W] = knapSackRe(W, wt, val, n - 1);
-            }
-            return dp[n][W];
+        if (wt[n] <= W) {
+            pick = val[n] + solve(W - wt[n], wt, val, n - 1);
         }
-        
+        notPick = solve(W, wt, val, n - 1);
+        return dp[n][W] = max(pick, notPick);
     }
-    int knapSack(int W, int wt[], int val[], int n) {
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= W; j++) {
-                dp[i][j] = -1;
-            }
-        }
-        return knapSackRe(W, wt, val, n);
+    int knapSack(int W, int wt[], int val[], int n)
+    {
+        // Your code here
+        memset(dp, -1, sizeof(dp));
+
+        return solve(W, wt, val, n - 1);
+       
     }
-    
 };
    
 //top down approach
@@ -70,20 +63,23 @@ class Solution
     { 
        // Your code here
        vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
-       for (int i = 1; i <= n; i++){
-           for (int j = 1; j <= W; j++){
-               int not_including = dp[i - 1][j];
-               int including = INT_MIN;
-               if (wt[i - 1] <= j){
-                   including = val[i - 1] + dp[i -1][j - wt[i - 1]];
-               }
-               dp[i][j] = max(including, not_including);
-           }
-       }
-       return dp[n][W];
+        for (int i = 0; i <= W; i++) {
+            if (wt[0] <= i) dp[0][i] = val[0];
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= W; j++) {
+                int pick = 0, notPick = 0;
+                if (wt[i] <= j) {
+                    pick = val[i] + dp[i - 1][j - wt[i]];
+                }
+                notPick = dp[i - 1][j];
+                dp[i][j] = max(pick, notPick);
+                
+            }
+        }
+        return dp[n - 1][W];
     }
-}
-
+};
 // space optimized solution
 // space: O(W)
 lass Solution
@@ -108,7 +104,7 @@ lass Solution
        return prev[W];
     }
 };
-// space Optimized to 1d
+// space: O(W)
 class Solution
 {
     public:
@@ -117,17 +113,21 @@ class Solution
     { 
        // Your code here
        vector<int> curr(W + 1, 0);
-       for (int i = 1; i <= n; i++){
-           curr[0] = 0;
-           for (int j = W; j >= 1; j--){
-               int including = INT_MIN;
-               if (wt[i - 1] <= j){
-                   including =   val[i - 1] + curr[j - wt[i - 1]];
-               }
-               curr[j] = max(curr[j], including);
-               
-           }
-       }
-       return curr[W];
+       vector<int> prev(W + 1, 0);
+        for (int i = 0; i <= W; i++) {
+            if (wt[0] <= i) prev[i] = val[0];
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= W; j++) {
+                int pick = 0, notPick = 0;
+                if (wt[i] <= j) {
+                    pick = val[i] + prev[j - wt[i]];
+                }
+                notPick = prev[j];
+                curr[j] = max(pick, notPick);
+            }
+            prev = curr;
+        }
+        return prev[W];
     }
 };
