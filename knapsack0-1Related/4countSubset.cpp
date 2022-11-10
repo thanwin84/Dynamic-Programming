367580c2cf1ee6fa19d45216c6803ed3db5c9906
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-11-10 14:32:47 +0600

    Update 4countSubset.cpp

--- knapsack0-1Related/4countSubset.cpp
+++ knapsack0-1Related/4countSubset.cpp
@@ -33,44 +33,23 @@ class Solution{
 
 
 //top down
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <unordered_map>
-#include <map>
-#include <queue>
-#include<set>
-#include<unordered_map>
-#include<string>
-#define mod 1000000007
+#include <bits/stdc++.h> 
 using namespace std;
-int dp[201][10002];
-class Solution {
-public:
-    int count_subset(vector<int>& nums, int sum) {
-      
-        
-        for (int i = 0; i <= sum; i++) dp[0][i] = 0;
-	// we can always make empty set 
-        for (int i = 0; i <= nums.size(); i++) dp[i][0] = 1;
-        for (int i = 1; i <= nums.size(); i++) {
-            for (int j = 1; j <= sum ; j++) {
-                if (nums[i - 1] <= j) {
-                    dp[i][j] = dp[i - 1][j - nums[i - 1]] + dp[i - 1][j];
-                }
-                else {
-                    dp[i][j] = dp[i - 1][j];
-                }
-            }
+int findWays(vector<int> &num, int tar)
+{
+    // Write your code here
+    int n = num.size();
+    vector<vector<int>> dp(n + 1, vector<int>(tar + 1, 0));
+    for (int i = 0; i <= n; i++) dp[i][0] = 1;
+    for (int ind = 1; ind <= n; ind++){
+        for (int sum = 0; sum <= tar; sum++){
+            int notTaken = dp[ind - 1][sum];
+            int taken = 0;
+            if (num[ind - 1] <= sum) taken = dp[ind - 1][sum - num[ind - 1]];
+            
+            dp[ind][sum] = taken + notTaken;
         }
-        return dp[nums.size()][sum];
-
     }
-};
-int main() {
-	Solution ob;
-    vector<int> nums = { 1, 2, 3, 3};
-    cout << ob.count_subset(nums, 6);
-   
-	return 0;
+    return dp[n][tar];
+    
 }
