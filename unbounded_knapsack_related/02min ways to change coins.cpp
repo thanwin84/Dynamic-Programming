a3ed574efd725a0b6b350919816d18a67701131d
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-11-16 00:48:48 +0600

    Update 02min ways to change coins.cpp

--- unbounded_knapsack_related/02min ways to change coins.cpp
+++ unbounded_knapsack_related/02min ways to change coins.cpp
@@ -1,89 +1,67 @@
 //memoization 
-int dp[13][10002];
+int dp[13][10001];
 class Solution {
 public:
-    int solve(vector<int> & coins, int amount, int n){
-        if (dp[n][amount] != -1){
-            return dp[n][amount];
+    int solve(vector<int> &coins, int amount, int N){
+        if (amount == 0) return 0;
+        if (N == 0) return 10001;
+        if (dp[N][amount] != -1) return dp[N][amount];
+        int included = solve(coins, amount, N - 1);
+        int notIncluded = 10001;
+        if (coins[N - 1] <= amount){
+            notIncluded = 1 + solve(coins, amount - coins[N - 1], N);
         }
-        if (amount == 0){
-            return 0;
-        }
-        if (n == 0){
-            return INT_MAX - 1;
-        }
-        if (coins[n -1] <= amount){
-            int included = 1 + solve(coins, amount - coins[n -1], n);
-            int not_included = solve(coins, amount, n - 1);
-            return dp[n][amount] =  min(included, not_included);
-        }
-        else {
-            return dp[n][amount] = solve(coins, amount, n -1);
-        }
+        return dp[N][amount] = min(included, notIncluded);
     }
     int coinChange(vector<int>& coins, int amount) {
         memset(dp, -1, sizeof(dp));
         int result = solve(coins, amount, coins.size());
-        if (result == INT_MAX -1){
+        if (result == 10001){
             return -1;
         }
         return result;
     }
 };
 //time O(n*m) and space: O(n*m)
-int dp[14][10002];
+int dp[13][10001];
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        for (int i = 0; i <= amount; i++) dp[0][i] = INT_MAX -1;
-        for (int i = 0; i <= coins.size(); i++) dp[i][0] = 0;
-        for (int i = 1; i <= amount; i++){
-            if (i % coins[0] == 0){
-                dp[1][i] = i / coins[0];
-            }
-            else{
-                dp[1][i] = INT_MAX - 1;
-            }
-        }
-        for (int i = 2; i <= coins.size(); i++){
+        int n = coins.size();
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, amount + 1));
+        for (int i = 1; i <= n; i++) dp[i][0] = 0;
+        for (int i = 1; i <= n; i++){
             for (int j = 1; j <= amount; j++){
-                if (coins[i -1] <= j){
-                    dp[i][j] = min(1 + dp[i][j - coins[i -1]], dp[i -1][j]);
-                }
-                else {
-                    dp[i][j] = dp[i -1][j];
-                }
+                int notIncluded = dp[i - 1][j];
+                int included = amount + 1;
+                if (coins[i - 1] <= j) included = 1 + dp[i][j - coins[i - 1]];
+                dp[i][j] = min(included, notIncluded);
             }
         }
-        if (dp[coins.size()][amount] == INT_MAX -1){
-            return -1;
-        }
-        return dp[coins.size()][amount];
+        int result = dp[n][amount];
+        if (result == amount + 1) return -1;
+        return result;
     }
 };
 //coin change for leetcode
 //space optimized
-int dp[10000];
+int dp[13][10001];
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        if (amount == 0){
-            return 0;
-        }
-        dp[0] = 0;
-        for (int i = 1; i <= amount; i++) dp[i] = INT_MAX -1;
-        for (int i = 1; i <= amount; i++) {
-            for (int j = 0; j < coins.size(); j++) {
-                if (coins[j] <= i) {
-                    dp[i] = min(dp[i], dp[i - coins[j]] + 1);
-                }
+        int n = coins.size();
+        vector<int> dp(amount + 1, amount + 1);
+        for (int i = 1; i <= n; i++){
+            dp[0] = 0;
+            for (int j = 1; j <= amount; j++){
+                int notIncluded = dp[j];
+                int included = amount + 1;
+                if (coins[i - 1] <= j) included = 1 + dp[j - coins[i - 1]];
+                dp[j] = min(included, notIncluded);
             }
         }
-        if (dp[amount] == INT_MAX -1) {
-            return -1;
-        }
-        else {
-            return dp[amount];
-        }
+        int result = dp[amount];
+        if (result == amount + 1) return -1;
+        return result;
     }
 };
