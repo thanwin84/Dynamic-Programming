d7e007e8cbfd823637549d533e4b09b23feff73c
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-11-16 10:08:16 +0600

    Update 03rodcutting.cpp

--- unbounded_knapsack_related/03rodcutting.cpp
+++ unbounded_knapsack_related/03rodcutting.cpp
@@ -1,53 +1,37 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <unordered_map>
-#include <map>
-#include <queue>
-#include<set>
-#include<unordered_map>
-#include<string>
-using namespace std;
-int dp[1002][1002];
-class Solution {
-public:
-    int rod_cutting(vector<int>& prices, vector<int> &lenghts, int length) {
-        int N = prices.size();
-        for (int i = 0; i <= length; i++) dp[0][i] = 0;
-        for (int i = 0; i <= N; i++) dp[i][0] = 0;
-        for (int i = 1; i <= N; i++) {
-            for (int j = 0; j <= length; j++) {
-                if (lenghts[i - 1] <= j) {
-                    dp[i][j] = max(prices[i - 1] + dp[i][j - lenghts[i - 1]], dp[i - 1][j]);
-                }
-                else {
-                    dp[i][j] = dp[i - 1][j];
-                }
+//memoized version
+class Solution{
+  public:
+    int cutRod(int price[], int n) {
+        //code here
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+        for (int ind = 1; ind <= n; ind++){
+            for (int target = 1; target <= n; target++){
+                int notPick = dp[ind - 1][target];
+                int pick = 0;
+                if (ind <= target) pick = price[ind- 1] + dp[ind][target - ind];
+                dp[ind][target] = max(pick, notPick);
             }
         }
-        return dp[N][N];
+        
+        return dp[n][n];
     }
 };
-int main() {
-    vector<int> l = { 1, 2, 3, 4, 5 , 6,  7,  8 };
-    vector<int> p = { 1, 5, 8, 9, 10, 17, 17, 20 };
-    Solution obj;
-    cout << obj.rod_cutting(p, l, 8)<< endl;
-	return 0;
-}
+//>>>>>> time: O(n * n) and space: O(n * n)
 //space optimized solution
 class Solution{
   public:
     int cutRod(int price[], int n) {
         //code here
-        int dp[n + 1];
-        memset(dp, 0, sizeof(dp));
-        for (int i = 0; i <= n; i++){
-            for (int j = 1; j <= i; j++){
-                dp[i] = max(dp[i], price[j - 1] + dp[i - j]);
+        vector<int> dp(n + 1, 0);
+        for (int ind = 1; ind <= n; ind++){
+            for (int target = 1; target <= n; target++){
+                int notPick = dp[target];
+                int pick = 0;
+                if (ind <= target) pick = price[ind- 1] + dp[target - ind];
+                dp[target] = max(pick, notPick);
             }
         }
+        
         return dp[n];
     }
 };
-
