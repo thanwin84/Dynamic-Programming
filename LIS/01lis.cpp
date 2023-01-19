412c2a4b47f3eac802e4b3e2bbf1a0d5ba431c86
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2023-01-19 19:50:32 +0600

    Update 01lis.cpp

--- LIS/01lis.cpp
+++ LIS/01lis.cpp
@@ -31,61 +31,56 @@ public:
     }
 };
 //time: O(n*n) and space: O(n)
-int dp[100002];
-class Solution
-{
+
+
+class Solution {
 public:
-    //Function to find length of longest increasing subsequence.
-    int longestSubsequence(int n, int a[])
-    {
-        // your code here
-        dp[0] = 1;
-        int result = 0;
-        for (int i = 1; i < n; i++) {
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n + 1, 0);
+        int _max = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
             dp[i] = 1;
-            for (int j = 0; j < i; j++) {
-                if (a[i] > a[j]) {
-                    int possible_ans = dp[j] + 1;
-                    dp[i] = max(dp[i], possible_ans);
+            for (int j = 1; j < i; j++){
+                if (nums[i - 1] > nums[j - 1]){
+                    int possibleAns = 1 + dp[j];
+                    dp[i] = max(dp[i], possibleAns);
                 }
             }
-            result = max(result, dp[i]);
+            _max = max(_max, dp[i]);
         }
-        return result;
+        return _max;
     }
 };
 //time: O(NlogN)
 class Solution {
 public:
-	int next_greater_element(vector<int>& arr, int target) {
-		int low = 0;
-		int high = arr.size();
-		int index = 0;
-		while (low <= high) {
-			int mid = low + (high - low) / 2;
-			if (arr[mid] >= target) {
-				index = mid;
-				// we can get better ans before mid
-				high = mid - 1;
-			}
-			else {
-				low = mid + 1;
-			}
-		}
-		return index;
-	}
-	int lengthOfLIS(vector<int>& arr) {
-		vector<int> li;
-		li.push_back(arr[0]);
-		for (int i = 1; i < arr.size(); i++) {
-            // if last index of li is less than arr[i], we can push arr[i] to li
-            // which will make LIS
-			if (li.back() < arr[i]) li.push_back(arr[i]);
-			else if (li.back() > arr[i]) {
-				int next_greater_index = next_greater_element(li, arr[i]);
-				li[next_greater_index] = arr[i];
-			}
-		}
-		return li.size();
-	}
+    int nextGreaterElement(vector<int> &nums, int target){
+        int low = 0;
+        int high = nums.size() - 1;
+        int ind = 0;
+        while (low <= high){
+            int mid = (low + high) / 2;
+            if (nums[mid] >= target){
+                ind = mid;
+                high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+        return ind;
+
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        for (int i = 1; i < nums.size(); i++){
+            if (temp.back() < nums[i]) temp.push_back(nums[i]);
+            else if (temp.back() > nums[i]){
+                int ind = nextGreaterElement(temp, nums[i]);
+                temp[ind] = nums[i];
+            }
+        }
+        return temp.size();
+    }
 };
