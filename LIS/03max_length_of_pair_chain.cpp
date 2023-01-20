8ab1b55ac9b69729c28af1d1105f9981ac0575af
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2023-01-20 22:14:42 +0600

    Update 03max_length_of_pair_chain.cpp

--- LIS/03max_length_of_pair_chain.cpp
+++ LIS/03max_length_of_pair_chain.cpp
@@ -1,26 +1,28 @@
 //time: O(n2)
-int dp[1002];
 class Solution {
 public:
-	int findLongestChain(vector<vector<int>>& pairs) {
-		sort(pairs.begin(), pairs.end());
-		dp[0] = 1;
-		int best_ans = 1;
-		for (int i = 1; i < pairs.size(); i++) {
-			dp[i] = 1;
-			for (int j = 0; j < i; j++) {
-				if (pairs[i][0] > pairs[j][1]) {
-					int possible_ans = 1 + dp[j];
-					dp[i] = max(dp[i], possible_ans);
-				}
-			}
-			best_ans = max(dp[i], best_ans);
-		}
-		return best_ans;
-		
-	}
+    int findLongestChain(vector<vector<int>>& pairs) {
+        int n = pairs.size();
+        vector<int> dp(n, 1);
+        dp[0] = 1;
+        // sort based on first index
+        sort(pairs.begin(), pairs.end());
+        int _max = 1;
+        for (int i = 1; i < n; i++){
+            for (int j = 0; j < i; j++){
+                if (pairs[j][1] < pairs[i][0]){
+                    int possibleAns = 1 + dp[j];
+                    dp[i] = max(possibleAns, dp[i]);
+                }
+            }
+            _max = max(_max, dp[i]);
+        }
+        return _max;
+        
+    }
 };
 //time: O(nlogn)
+// greedy approach
 bool comp(vector<int> &p1, vector<int> &p2){
     return p1[1] < p2[1];
 }
