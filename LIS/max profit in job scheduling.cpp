424234fb69b73aea2de4822b375a1dae666cae85
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2023-01-21 23:42:10 +0600

    Update max profit in job scheduling.cpp

--- LIS/max profit in job scheduling.cpp
+++ LIS/max profit in job scheduling.cpp
@@ -1,40 +1,44 @@
-//time: O(n^2)
-int dp[50009];
-struct Pair {
-	int start_time;
-	int end_time;
-	int profit;
-	Pair(int end_time, int start_time, int profit) {
-		this->start_time = start_time;
-		this->end_time = end_time;
-		this->profit = profit;
-	}
+//time: O(n^2) 
+// TLE
+class Job {
+public:
+    int startTime;
+    int endTime;
+    int profit;
+    Job(int startTime, int endTime, int profit) {
+        this->startTime = startTime;
+        this->endTime = endTime;
+        this->profit = profit;
+    }
 };
-bool comp(Pair p1, Pair p2) {
-	return p1.end_time < p2.end_time;
+bool comp(Job p1, Job p2) {
+    return p1.startTime < p2.startTime;
 }
 class Solution {
 public:
-	int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
-		vector<Pair> schedules;
-		for (int i = 0; i < profit.size(); i++) {
-			schedules.push_back(Pair( endTime[i], startTime[i], profit[i]));
-		}
-		sort(schedules.begin(), schedules.end(), comp);
-		dp[0] = schedules[0].profit;
-		int max_profit = dp[0];
-		for (int i = 1; i < startTime.size(); i++) {
-			dp[i] = schedules[i].profit;
-			for (int j = 0; j < i; j++) {
-				if (schedules[j].end_time <= schedules[i].start_time) {
-					int possible_ans = dp[j] + schedules[i].profit;
-					dp[i] = max(possible_ans, dp[i]);
-				}
-			}
-			max_profit = max(max_profit, dp[i]);
-		}
-		return max_profit;
-	}
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        int size = startTime.size();
+        vector<int> dp(size, 0);
+        vector<Job> jobs;
+        for (int i = 0; i < size; i++) {
+            jobs.push_back(Job(startTime[i], endTime[i], profit[i]));
+        }
+        // sort according to start time
+        sort(jobs.begin(), jobs.end(), comp);
+        dp[0] = jobs[0].profit;
+        int maxProfit = dp[0];
+        for (int i = 1; i < size; i++) {
+            dp[i] = jobs[i].profit;
+            for (int j = 0; j < i; j++) {
+                if (jobs[i].startTime >= jobs[j].endTime) {
+                    int possibleProfit = dp[j] + jobs[i].profit;
+                    dp[i] = max(possibleProfit, dp[i]);
+                }
+            }
+            maxProfit = max(maxProfit, dp[i]);
+        }
+        return maxProfit;
+    }
 };
 //time: O(nlogn)
 int dp[100000];
