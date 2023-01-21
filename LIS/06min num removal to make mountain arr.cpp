024fa87fe756c81db234d3ac5979d7a8e9c10965
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2023-01-21 12:28:40 +0600

    Update 06min num removal to make mountain arr.cpp

--- LIS/06min num removal to make mountain arr.cpp
+++ LIS/06min num removal to make mountain arr.cpp
@@ -1,44 +1,34 @@
 //time: O(n^2) and space: O(n)
-using namespace std;
 class Solution {
 public:
-	vector<int> LIS(vector<int> &arr)
-	{
-		// your code here
-		int n = arr.size();
-		vector<int> dp(n + 1, 0);
-		dp[0] = 1;
-		int best = 0;
-		for (int i = 1; i < n; i++) {
-			dp[i] = 1;
-			for (int j = 0; j < i; j++) {
-				if (arr[j] > arr[i]) {
-					continue;
-				}
-				else if (arr[i] > arr[j]) {
-					int possibleAns = dp[j] + 1;
-					if (possibleAns > dp[i]) dp[i] = possibleAns;
-					if (dp[i] > best) best = dp[i];
-				}
-			}
-		}
-		return dp;
-	}
-	int minimumMountainRemovals(vector<int>& arr) {
-		vector<int> reversed_arr;
-		for (int i = arr.size() - 1; i >= 0; i--) reversed_arr.push_back(arr[i]);
-		vector<int> dp1 = LIS(arr);
-		vector<int> dp2 = LIS(reversed_arr);
-		int ans = INT_MIN;
-		for (int i = 1; i < arr.size() - 1; i++) {
-            if (dp1[i] > 1 and dp2[arr.size() - i -1] > 1){
-                int temp =dp1[i] + dp2[arr.size() - i - 1] - 1;
-			    ans = max(ans, temp);
+    int minimumMountainRemovals(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> dp1(n, 1);
+        vector<int> dp2(n, 1);
+        int _max = 0;
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < i; j++){
+                if (arr[i] > arr[j]){
+                    int possibleAns = 1 + dp1[j];
+                    dp1[i] = max(possibleAns, dp1[i]);
+                }
             }
-		}
-		return arr.size() - ans;
-	}
-	
+        }
+        for (int i = n - 1; i >= 0; i--){
+            for (int j = n - 1; j > i; j--){
+                if (arr[i] > arr[j]){
+                    int possibleAns = 1 + dp2[j];
+                    dp2[i] = max(possibleAns, dp2[i]);
+                } 
+            }
+	    // we have to make sure that at every peak index we have mountain array
+            if (dp1[i] > 1 && dp2[i] > 1){
+                _max = max(_max, dp1[i] + dp2[i] - 1);
+            }
+            
+        }
+        return n - _max;
+    }
 };
 //time: O(nlogn)
 class Solution {
