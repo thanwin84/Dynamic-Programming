3ed1df2a7971ea48bfed3f9fdb279fae759dedf4
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2023-01-21 10:43:33 +0600

    Update printShortestSupersequence.cpp

--- LCS_related/printShortestSupersequence.cpp
+++ LCS_related/printShortestSupersequence.cpp
@@ -1,23 +1,12 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <unordered_map>
-#include <map>
-#include <queue>
-#include<set>
-#include<unordered_map>
-#include<string>
-using namespace std;
-int dp[10000][10000];
-
 class Solution {
 public:
     string shortestCommonSupersequence(string str1, string str2) {
-        for (int i = 0; i <= str1.size(); i++) dp[i][0] = 0;
-        for (int i = 0; i <= str2.size(); i++) dp[0][i] = 0;
-        for (int i = 1; i <= str1.size(); i++) {
-            for (int j = 1; j <= str2.size(); j++) {
-                if (str1[i - 1] == str2[j - 1]) {
+        int n1 = str1.size();
+        int n2 = str2.size();
+        vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, 0));
+        for (int i = 1; i <= n1; i++){
+            for (int j = 1; j <= n2; j++){
+                if (str1[i - 1] == str2[j - 1]){
                     dp[i][j] = 1 + dp[i - 1][j - 1];
                 }
                 else {
@@ -26,30 +15,28 @@ public:
             }
         }
         string result;
-        int i = str1.size();
-        int j = str2.size();
-        while (i > 0 and j > 0) {
-            if (str1[i - 1] == str2[j - 1]) {
+        int i = n1;
+        int j = n2;
+        while (i > 0 && j > 0){
+            if (str1[i - 1] == str2[j - 1]){
                 result += str1[i - 1];
                 i--;
                 j--;
             }
+            else if (dp[i - 1][j] >= dp[i][j - 1]){
+                result += str1[i - 1];
+                i--;
+            }
             else {
-                if (dp[i][j - 1] > dp[i - 1][j]) {
-                    result += str2[j - 1];
-                    j--;
-                }
-                else {
-                    result += str1[i - 1];
-                    i--;
-                }
+                result += str2[j - 1];
+                j--;
             }
         }
         while (i > 0) {
             result += str1[i - 1];
             i--;
         }
-        while (j > 0) {
+        while (j > 0){
             result += str2[j - 1];
             j--;
         }
@@ -57,13 +44,3 @@ public:
         return result;
     }
 };
-int main() {
-    string a = "abac";
-    string b = "cab";
-    Solution obj;
-    string result = obj.shortestCommonSupersequence(a, b);
-    for (auto i : result) {
-        cout << i << " ";
-    }
-    return 0;
-}
