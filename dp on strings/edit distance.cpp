d8f417e0fcd44698dc6aa9b4d7f907ff29475454
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2023-02-21 16:52:39 +0600

    Update edit distance.cpp

--- dp on strings/edit distance.cpp
+++ dp on strings/edit distance.cpp
@@ -1,26 +1,30 @@
-//memoization 
-//time and space:  O(n * m)
-int dp[502][502];
+///////////////////memoization///////////////////////////
+
+int dp[501][501];
 class Solution {
 public:
     int solve(string &word1, string &word2, int i, int j){
-        if (i == 0){
-            return j;
-        }
-        if (j == 0){
-            return i;
-        }
+        // if the first string is empty, the only option left 
+        // is to insert all characters from second string
+        if (i == 0) return j;
+        // if the second string empty, then we have to delete all the chars
+        // in the first string
+        if (j == 0) return i;
+        
         if (dp[i][j] != -1){
             return dp[i][j];
         }
-        if (word1[i-1] == word2[j -1]){
-            return dp[i][j] = solve(word1, word2, i-1, j -1);
+        // if both character is same, there is nothing to do
+        // just solve the remaining part
+        if (word1[i - 1] == word2[j - 1]){
+            return solve(word1, word2, i - 1, j - 1);
         }
         else {
-            int insert = 1 + solve(word1, word2, i, j - 1);
-            int _delete = 1 + solve(word1, word2, i - 1, j);
-            int transition = 1 + solve(word1, word2, i - 1, j - 1);
-            return dp[i][j] = min(min(insert, _delete), transition);
+            // if not same, we can try any of these 3 operations
+            int insertion = 1 + solve(word1, word2, i, j- 1);
+            int deletion = 1 + solve(word1, word2, i - 1, j);
+            int replace = 1 + solve(word1, word2, i - 1, j - 1);
+            return dp[i][j] = min(insertion, min(deletion, replace));
         }
     }
     int minDistance(string word1, string word2) {
@@ -28,29 +32,33 @@ public:
         return solve(word1, word2, word1.size(), word2.size());
     }
 };
-int dp[10000][10000];
+// Time Complexity: O(n *m)
+// space: O(n * m)
+/////////////////////////// TOP DOWN APPROACH /////////
 
 class Solution {
 public:
     int minDistance(string word1, string word2) {
-        for (int i = 0; i <= word1.size(); i++) dp[i][0] = i;
-        for (int i = 0; i <= word2.size(); i++) dp[0][i] = i;
-        for (int i = 1; i <= word1.size(); i++) {
-            for (int j = 1; j <= word2.size(); j++) {
-                if (word1[i - 1] == word2[j - 1]) {
+        vector<vector<int>> dp(word1.size() + 1, vector<int>(word2.size() + 1, 0));
+        for (int i = 0; i <= word1.size(); i++){
+            for (int j = 0; j <= word2.size(); j++){
+                if (i == 0) dp[i][j] = j;
+                else if (j == 0) dp[i][j] = i;
+                else if (word1[i - 1] == word2[j - 1]){
                     dp[i][j] = dp[i - 1][j - 1];
                 }
                 else {
-                    int add = dp[i][j - 1] + 1;
-                    int deletion = dp[i - 1][j] + 1;
-                    int transition = dp[i - 1][j - 1] + 1;
-                    dp[i][j] = min(add, min(deletion, transition));
+                    int insertion = 1 + dp[i][j - 1];
+                    int deletion = 1 + dp[i - 1][j];
+                    int replace = 1 + dp[i - 1][j - 1];
+                    dp[i][j] = min(insertion, min(deletion, replace));
                 }
             }
         }
         return dp[word1.size()][word2.size()];
     }
 };
+//////// 1d space solution////////////
 // space: O(len(str2)
 class Solution {
 public:
