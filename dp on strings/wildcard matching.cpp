05c9e90f98880aeb54b8d3563edfef21335a9fc4
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2023-02-21 21:16:32 +0600

    Update wildcard matching.cpp

--- dp on strings/wildcard matching.cpp
+++ dp on strings/wildcard matching.cpp
@@ -48,69 +48,101 @@ public:
 // time: O(n *m) and space: O(n * m)
 
 ///////////////////////iterative approach/////////////////////
-int dp[2001][2001];
+
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        dp[0][0] = true;
-        for (int i = 1; i <= s.size(); i++) dp[i][0] = false;
-        for (int i = 1; i <= p.size(); i++){
-            bool flag = true;
-            for (int j = 1; j <= i; j++){
-                if (p[j -1] != '*'){
-                    flag = false;
-                    break;
+        int p_size = p.size();
+        int s_size = s.size();
+        vector<vector<int>> dp(p_size + 1, vector<int>(s_size + 1, 0));
+        for (int i = 0; i <= p_size; i++){
+            for (int j = 0; j <= s_size; j++){
+                 // both strings are empty
+                // that means two string are same
+                if (i == 0 && j == 0){
+                    dp[i][j] = true;
                 }
-            }
-            dp[0][i] = flag;
-        }
-        for (int i = 1; i <= s.size(); i++){
-            for (int j = 1; j <= p.size(); j++){
-                if (s[i - 1] == p[j -1] || p[j -1] == '?'){
-                    dp[i][j] = dp[i - 1][j -1];
-                }
-                else if (p[j -1] == '*'){
-                    dp[i][j] = dp[i - 1][j] || dp[i][j -1];
-                }
-                else {
+                // patern is empty and other string is not empty
+                // that means this patter doesn't match other string
+                else if (i == 0 && j > 0){
                     dp[i][j] = false;
                 }
+                // if all the character in pattern is *
+                // then we can match the empty string with this pattern
+                // if any character in this pattern is not equal to *, then 
+                // we can't match
+                else if (j == 0 && i > 0){
+                    bool flag = true;
+                    for (int c = 1; c <= i; c++){
+                        if (p[c - 1] != '*'){
+                            flag = false;
+                            break;
+                        }
+                    }
+                    dp[i][j] = flag;
+                }
+                // if both character is same, we can match
+            // if last char of patter is ?, then we can match with single character
+                else if (p[i - 1] == s[j - 1] || p[i - 1] == '?'){
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                // we can match with 0 characters or more
+                else if (p[i - 1] == '*'){
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+                else dp[i][j] = false;
             }
         }
-        return dp[s.size()][p.size()];
+        return dp[p_size][s_size];
     }
 };
-// space pptimized to 1d
+//////////////////////////space pptimized to 1d
+
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        vector<int> curr(p.size()+ 1), prev(p.size() + 1);
-        prev[0] = true;
-        for (int i = 1; i <= p.size(); i++){
-            bool flag = true;
-            for (int j = 1; j <= i; j++){
-                if (p[j -1] != '*'){
-                    flag = false;
-                    break;
+        int p_size = p.size();
+        int s_size = s.size();
+        vector<int> prev(s_size + 1, 0), curr(s_size + 1, 0);
+        for (int i = 0; i <= p_size; i++){
+            for (int j = 0; j <= s_size; j++){
+                 // both strings are empty
+                // that means two string are same
+                if (i == 0 && j == 0){
+                    curr[j] = true;
                 }
-            }
-            prev[i] = flag;
-        }
-        for (int i = 1; i <= s.size(); i++){
-            curr[0] = false;
-            for (int j = 1; j <= p.size(); j++){
-                if (s[i - 1] == p[j -1] || p[j -1] == '?'){
-                    curr[j] = prev[j -1];
-                }
-                else if (p[j -1] == '*'){
-                    curr[j] = prev[j] || curr[j -1];
-                }
-                else {
+                // patern is empty and other string is not empty
+                // that means this patter doesn't match other string
+                else if (i == 0 && j > 0){
                     curr[j] = false;
                 }
+                // if all the character in pattern is *
+                // then we can match the empty string with this pattern
+                // if any character in this pattern is not equal to *, then 
+                // we can't match
+                else if (j == 0 && i > 0){
+                    bool flag = true;
+                    for (int c = 1; c <= i; c++){
+                        if (p[c - 1] != '*'){
+                            flag = false;
+                            break;
+                        }
+                    }
+                    curr[j] = flag;
+                }
+                // if both character is same, we can match
+                 // if last char of patter is ?, then we can match with single character
+                else if (p[i - 1] == s[j - 1] || p[i - 1] == '?'){
+                    curr[j] = prev[j - 1];
+                }
+                // we can match with 0 characters or more
+                else if (p[i - 1] == '*'){
+                    curr[j] = prev[j] || curr[j - 1];
+                }
+                else curr[j] = false;
             }
             prev = curr;
         }
-        return prev[p.size()];
+        return curr[s_size];
     }
 };
