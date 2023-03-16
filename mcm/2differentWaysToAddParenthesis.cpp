3ecc8decaffd063b920319de062df93282f71eef
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2023-03-16 10:58:24 +0600

    Update 2differentWaysToAddParenthesis.cpp

--- mcm/2differentWaysToAddParenthesis.cpp
+++ mcm/2differentWaysToAddParenthesis.cpp
@@ -1,45 +1,34 @@
-unordered_map<string, vector<int>> dp;
 class Solution {
+    unordered_map<string, vector<int>> seen;
 public:
-	vector<int> diffWaysToCompute(string expression) {
-		if (dp.count(expression)) {
-			return dp[expression];
-		}
-		vector<int> result, left, right;
-		int num = 0;
-		int size = expression.size();
-		for (int i = 0; i < size; i++) {
-			char c = expression[i];
-			if (c != '*' and c != '+' and c != '-') {
-				num = num * 10 + c - '0';
-			}
-			else {
-				if (dp.count(expression.substr(0, i))) {
-					left = dp[expression.substr(0, i)];
-				}
-				else {
-					left = diffWaysToCompute(expression.substr(0, i));
-				}
-				if (dp.count(expression.substr(i + 1))) {
-					right = dp[expression.substr(i + 1)];
-				}
-				else {
-					right = diffWaysToCompute(expression.substr(i + 1));
-				}
-				for (auto l : left) {
-					for (auto r : right) {
-						if (c == '*') result.push_back(l * r);
-						else if (c == '+') result.push_back(l + r);
-						else if (c == '-') result.push_back(l - r);
-					}
-				}
-			}
-		}
-		if (result.size() == 0) {
-			result.push_back(num);
-		}
-		dp[expression] = result;
-		return result;
-
-	}
+    vector<int> diffWaysToCompute(string expression){
+        if (seen.count(expression)) return seen[expression];
+        // we have only single number
+        vector<int> result, left, right;
+        int num = 0;
+        for (int i = 0; i < expression.size(); i++){
+            char ch = expression[i];
+            if (ch != '+' && ch != '-' && ch != '*'){
+                num = (num * 10) + ch - '0';
+            }
+            else {
+                left = diffWaysToCompute(expression.substr(0, i));
+                right = diffWaysToCompute(expression.substr(i + 1));
+            }
+            for (auto l: left){
+                for (auto r: right){
+                    if (ch == '+') result.push_back(l + r);
+                    else if (ch == '*') result.push_back(l * r);
+                    else if (ch == '-') result.push_back(l - r);
+                }
+            }
+        }
+        // base case
+	// single number
+        if (result.size() == 0){
+            result.push_back(num);
+        }
+        seen[expression] = result;
+        return result;
+    }
 };
