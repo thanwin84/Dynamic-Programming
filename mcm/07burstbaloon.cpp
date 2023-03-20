96116286b8e36188fdc04ad5ea143eab16f51204
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2023-03-20 22:23:43 +0600

    Update 07burstbaloon.cpp

--- mcm/07burstbaloon.cpp
+++ mcm/07burstbaloon.cpp
@@ -1,32 +1,22 @@
-int dp[501][501];
+int dp[301][301];
 class Solution {
 public:
-    int solve(vector<int>& nums, int i, int j) {
-        if (i > j) {
-            return 0;
+    int solve(vector<int> &nums, int start, int end){
+        if (start > end) return 0;
+        if (dp[start][end] != -1) return dp[start][end];
+        int ans = 0;
+        for (int partitionIndex = start; partitionIndex <= end; partitionIndex++){
+            int left_ind = (start - 1 < 0) ? 1: nums[start - 1];
+            int right_ind = (end + 1 >= nums.size()) ? 1: nums[end + 1];
+            int cost = left_ind * nums[partitionIndex] * right_ind;
+            int left = solve(nums, start, partitionIndex - 1);
+            int right = solve(nums, partitionIndex + 1, end);
+            ans = max(ans, cost + left + right);
         }
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-        int ans = INT_MIN;
-        for (int k = i; k <= j; k++) {
-            int l, r;
-            if (i == 0) l = 1;
-            else l = nums[i - 1];
-            if (j == nums.size() - 1) r = 1;
-            else r = nums[j + 1];
-            int left = solve(nums, i, k - 1);
-            int right = solve(nums, k + 1, j);
-            int temp = left + right + (l * nums[k] * r);
-            ans = max(temp, ans);
-        }
-        return dp[i][j] = ans;
+        return dp[start][end] = ans;
     }
     int maxCoins(vector<int>& nums) {
-        int i = 0;
-        int j = nums.size() - 1;
         memset(dp, -1, sizeof(dp));
-        return solve(nums, i, j);
-
+        return solve(nums, 0, nums.size() - 1);
     }
 };
